b70b382a17576497e72ac8bb0bd78ac0c6956aaa
FraBif <francescobif@hotmail.it>
2024-01-10 23:43:20 +0100

    Added style to the cats list

--- src/app/cats/cats.component.ts
+++ src/app/cats/cats.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CatsServiceService } from '../services/cats-service/cats-service.service';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider'; 
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-cats',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatDividerModule, MatListModule],
   templateUrl: './cats.component.html',
   styleUrl: './cats.component.css'
 })
